bd58881692b4c81ef2523e0373e3a403eaaf3671
Jamil Faris <jamilfaris2000@gmail.com>
2026-03-09 08:52:28 +0300

    Fix: The Juz's starts are now correct

--- lib/data/juz_data.dart
+++ lib/data/juz_data.dart
@@ -2,9 +2,9 @@
 // Each entry is (surahIndex, ayah) where that juz' starts.
 const List<({int surah, int ayah})> kJuzStarts = [
   (surah: 1, ayah: 1),   // Juz 1
-  (surah: 2, ayah: 142), // Juz 2
-  (surah: 2, ayah: 253), // Juz 3
-  (surah: 3, ayah: 92),  // Juz 4
+  (surah: 1, ayah: 142), // Juz 2
+  (surah: 1, ayah: 253), // Juz 3
+  (surah: 2, ayah: 92),  // Juz 4
   (surah: 4, ayah: 24),  // Juz 5
   (surah: 4, ayah: 148), // Juz 6
   (surah: 5, ayah: 82),  // Juz 7
@@ -12,24 +12,24 @@ const List<({int surah, int ayah})> kJuzStarts = [
   (surah: 7, ayah: 88),  // Juz 9
   (surah: 8, ayah: 41),  // Juz 10
   (surah: 9, ayah: 93),  // Juz 11
-  (surah: 10, ayah: 26), // Juz 12
-  (surah: 11, ayah: 6),  // Juz 13
-  (surah: 12, ayah: 53), // Juz 14
-  (surah: 15, ayah: 2),  // Juz 15
-  (surah: 17, ayah: 1),  // Juz 16
-  (surah: 18, ayah: 75), // Juz 17
-  (surah: 21, ayah: 1),  // Juz 18
-  (surah: 23, ayah: 1),  // Juz 19
-  (surah: 25, ayah: 21), // Juz 20
-  (surah: 27, ayah: 56), // Juz 21
-  (surah: 29, ayah: 46), // Juz 22
-  (surah: 33, ayah: 31), // Juz 23
-  (surah: 36, ayah: 28), // Juz 24
-  (surah: 39, ayah: 32), // Juz 25
-  (surah: 41, ayah: 47), // Juz 26
-  (surah: 46, ayah: 1),  // Juz 27
-  (surah: 51, ayah: 31), // Juz 28
-  (surah: 58, ayah: 1),  // Juz 29
+  (surah: 11, ayah: 6),  // Juz 12
+  (surah: 12, ayah: 53), // Juz 13
+  (surah: 15, ayah: 1),  // Juz 14
+  (surah: 17, ayah: 1),  // Juz 15
+  (surah: 18, ayah: 75), // Juz 16
+  (surah: 21, ayah: 1),  // Juz 17
+  (surah: 23, ayah: 1),  // Juz 18
+  (surah: 25, ayah: 21), // Juz 19
+  (surah: 27, ayah: 56), // Juz 20
+  (surah: 29, ayah: 46), // Juz 21
+  (surah: 33, ayah: 31), // Juz 22
+  (surah: 36, ayah: 28), // Juz 23
+  (surah: 39, ayah: 32), // Juz 24
+  (surah: 41, ayah: 47), // Juz 25
+  (surah: 46, ayah: 1),  // Juz 26
+  (surah: 51, ayah: 31), // Juz 27
+  (surah: 58, ayah: 1),  // Juz 28
+  (surah: 67, ayah: 1),  // Juz 29
   (surah: 78, ayah: 1),  // Juz 30
 ];
 
